44047dabd5e97c3ee1ef7b3b71e3423dc53ac457
Sergij Pankiv <pankivsergij97@gmail.com>
2024-01-23 13:53:10 +0200

    Init commit

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href='#' className='logo'>
+          BlogLogo
+        </a>
+        <nav className='navigation'>
+          <a href='#' className='login'>
+            Login
+          </a>
+          <a href='#' className='register'>
+            Register
+          </a>
+        </nav>
+      </header>
+      <div className='post'>
+        <div className='image'>
+          <img
+            className='post-image'
+            src='../assets/images/cat1.jpg'
+            alt='cat'
+          />
+        </div>
+        <div className='post-texts'>
+          <h2 className='post-title'>Post title</h2>
+          <p className='info'>
+            <a className='author'> David Pazko</a>
+            <time>23-01-06 16:45</time>
+          </p>
+          <p className='summary'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem,
+            incidunt commodi tenetur sed odio minus dicta aliquid laborum eos ab
+            beatae quo! Optio esse impedit vero assumenda earum doloribus quod?
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
