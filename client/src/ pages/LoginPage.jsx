5b277437c0c8c9fffc8b3dc542dd42f093910462
Sergij Pankiv <pankivsergij97@gmail.com>
2024-01-23 19:59:42 +0200

    solution was added

--- client/src/ pages/LoginPage.jsx
+++ client/src/ pages/LoginPage.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <form className='form-login' action=''>
+      <h1 className='form-login_title'>Login</h1>
+      <input className='form-login_input' type='text' placeholder='username' />
+      <input
+        className='form-login_input'
+        type='password'
+        placeholder='password'
+      />
+      <button className='form-login_button'>Login</button>
+    </form>
+  );
 }
--- client/src/ pages/RegisterPage.jsx
+++ client/src/ pages/RegisterPage.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <form className='form-register' action=''>
+      <h1 className='form-lregister_title'>Register</h1>
+      <input
+        className='form-register_input'
+        type='text'
+        placeholder='username'
+      />
+      <input
+        className='form-register_input'
+        type='password'
+        placeholder='password'
+      />
+      <button className='form-register_button'>Register</button>
+    </form>
+  );
 }
